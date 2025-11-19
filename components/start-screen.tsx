65f3435b7b3a4e54a6ab48238efe807007c3340a
Peter Sloan <peter.sloan@TRA419.local>
2025-11-20 01:42:38 +1300

    Remove unused useState import

--- components/start-screen.tsx
+++ components/start-screen.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { useState } from "react"
 import { Button } from "@/components/ui/button"
 import { useAudioManager } from "@/hooks/use-audio-manager"
 import Menu from "./menu"
